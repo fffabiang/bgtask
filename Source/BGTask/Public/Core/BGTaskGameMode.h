9032b9309e512a4726377284a118218fa88d8b64
Francisco Fabian <30538633+fffabiang@users.noreply.github.com>
2024-03-16 14:25:47 -0500

    Third person template setup with some base classes

---
+++ Source/BGTask/Public/Core/BGTaskGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BGTaskGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABGTaskGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABGTaskGameMode();
+};
+
+
+
---
+++ Source/BGTask/Public/Core/BGTaskPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BGTaskPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BGTASK_API ABGTaskPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
